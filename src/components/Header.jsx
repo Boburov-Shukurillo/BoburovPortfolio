372944cd45c77f6bcf31a45000709bfdfb005257
Boburov-Shukurillo <boburovshukurilo@gmail.com>
2024-04-02 11:34:38 +0500

    fix performans

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,29 +29,50 @@ const Header = () => {
         <div className="containerb flex items-center justify-between h-14">
           <Link className="flex items-center justify-between w-1/2">
             <h1 className="text-2xl text-white w-full leading-5 myFont">
-              <Link to='/'>Boburov.Dev</Link>
+              <Link to="/">Boburov.Dev</Link>
             </h1>
           </Link>
           <div className="w-full flex items-center justify-end gap-x-20 max-md:justify-end max-md:mr-5">
             <nav className="w-1/4 max-mini_screen:hidden">
-              <ul className="flex items-center justify-end gap-x-10 text-white font-medium">
-                <li className="cursor-pointer">
-                  <a href="#home">Home</a>
+              <ul className="flex items-center justify-end gap-x-10">
+                <li>
+                  <a
+                    className="cursor-pointer text-white font-medium"
+                    href="#home"
+                  >
+                    Home
+                  </a>
                 </li>
-                <li className="cursor-pointer">
-                  <a href="#about">About</a>
+                <li>
+                  <a
+                    className="cursor-pointer text-white font-medium"
+                    href="#about"
+                  >
+                    About
+                  </a>
                 </li>
-                <li className="cursor-pointer">
-                  <a href="#projects">Projects</a>
+                <li>
+                  <a
+                    className="cursor-pointer text-white font-medium"
+                    href="#projects"
+                  >
+                    Projects
+                  </a>
                 </li>
-                <li className="cursor-pointer">
-                  <a href="#contact">Contact</a>
+                <li>
+                  <a
+                    className="cursor-pointer text-white font-medium"
+                    href="#contact"
+                  >
+                    Contact
+                  </a>
                 </li>
               </ul>
             </nav>
           </div>
 
           <button
+            id="menuBtn"
             onClick={() => setMenu("left-0")}
             className="transition-all hidden max-mini_screen:block"
           >
